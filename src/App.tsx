43de6f850d38b2862e819e3d0cdaba3713cd4ed3
hdd1013 <hdd113@outlook.com>
2024-10-04 14:02:23 +0900

    Bodyタイトル作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import View from './components/layout/View';
 import Body from './components/layout/Body';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquarePollVertical } from '@fortawesome/free-solid-svg-icons';
+
 function App () {
 
   return (
     <View>
-      <Body>Body</Body>
+      <Body>
+        <div className="flex-auto flex flex-col p-10">
+          {/* Body Title */}
+          <div className="flex-0 pb-5 flex border-b border-border">
+            <div className="flex flex-row text-white items-end">
+              <div className="flex flex-row items-center mt-1 leading-none">
+                <FontAwesomeIcon icon={faSquarePollVertical} className='text-2xl -mb-1' height={'0.95em'}/>
+                <h2 className='text-2xl ml-2'>取引価格</h2>
+              </div>
+              <span className='text-sm ml-4 leading-none'>※取引面積1㎡あたり</span>
+            </div>
+            <div className=""></div>
+          </div>
+        </div>
+      </Body>
     </View>
   );
 }
--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -2,7 +2,7 @@ import backgroundImage from '../../assets/background.webp';
 
 function Body ({ children }:{ children:React.ReactNode }) {
   return (
-    <div className="flex-auto bg-cover bg-center" style={{
+    <div className="flex flex-col flex-auto bg-cover bg-center" style={{
       backgroundImage: `url(${backgroundImage})`,
     }}>
       {children}
