792e9794846d7e3a7ead00c6b4583e39d00ef9db
hdd1013 <hdd113@outlook.com>
2024-10-04 19:10:49 +0900

    ラジオボタンコンポネント

---
+++ src/components/input/LanditRadio.tsx
@@ -0,0 +1,29 @@
+import styleSheet from './LanditRadio.module.scss';
+
+function LanditRadio ({ name, value, id, className, onChange, style, checked, disabled }:{
+  name?:string,
+  value?:string,
+  id?:string,
+  className?:string,
+  style?:React.CSSProperties,
+  onChange?:React.ChangeEventHandler<HTMLInputElement>,
+  checked?:boolean,
+  disabled?:boolean,
+}) {
+  return (
+    <input type="radio"
+      name={name}
+      id={id}
+      value={value}
+      className={`${styleSheet.radio} ${className}`}
+      onChange={onChange}
+      checked={checked}
+      disabled={disabled}
+      style={{
+        ...style,
+      }}
+    />
+  );
+}
+
+export default LanditRadio;
